2ac5dab42808d94669ed654c58a496a0977f48b0
Nimatrixx <www.nimatrix@gmail.com>
2025-03-10 19:19:48 +0330

    style(name): rename project name to pisaz

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Pisaz());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Pisaz extends StatelessWidget {
+  const Pisaz({super.key});
 
   @override
   Widget build(BuildContext context) {

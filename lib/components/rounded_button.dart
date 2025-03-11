2329dc857667c43fb8c49fefd237dc7aaf93e969
Nimatrixx <www.nimatrix@gmail.com>
2025-03-11 16:16:56 +0330

    feat(button): add custom rounded button component

---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton(
+      {super.key,
+      required this.onPressed,
+      this.width,
+      this.height,
+      this.color,
+      this.text,
+      this.textColor});
+
+  final double? width;
+  final double? height;
+  final String? text;
+  final Color? color;
+  final Color? textColor;
+  final Function() onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(8.0),
+        elevation: 8.0,
+        child: MaterialButton(
+          minWidth: width,
+          height: height,
+          onPressed: onPressed,
+          child: Text(
+            text ?? 'Button',
+            style: TextStyle(
+              color: textColor,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
